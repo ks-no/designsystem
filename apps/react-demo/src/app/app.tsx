230c65a1ff857c951681c9e0ee997f0b0e346baa
stianmorsund <stian.morsund@ks.no>
2025-05-05 12:55:50 +0200

    chore(react-demo): test button

--- apps/react-demo/src/app/app.tsx
+++ apps/react-demo/src/app/app.tsx
@@ -1,12 +1,12 @@
-// import './ledsagerbevis.css'
-// import '@digdir/designsystemet-css'
-
-import { Alert } from '@ks-no/designsystem-react'
+import { Alert, Button } from '@ks-no/designsystem-react'
 
 export function App() {
   return (
     <div>
       Hi
+      <Button variant="primary" data-size="lg">
+        Hio
+      </Button>
       <Alert data-color="info">En beskjed det er viktig at brukeren ser</Alert>
     </div>
   )
