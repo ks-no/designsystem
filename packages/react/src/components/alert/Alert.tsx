1833bb2566fa28e082cddcdd5f5b7a99e43f3df4
stianmorsund <stian.morsund@ks.no>
2025-05-05 10:36:29 +0200

    chore: remove comment

--- packages/react/src/components/alert/Alert.tsx
+++ packages/react/src/components/alert/Alert.tsx
@@ -1,6 +1,2 @@
 import { Alert, type AlertProps } from '@digdir/designsystemet-react'
 export { Alert, AlertProps }
-
-// export function Alert() {
-//   return <div style={{ background: '#ffcc00' }}>My alert</div>
-// }
