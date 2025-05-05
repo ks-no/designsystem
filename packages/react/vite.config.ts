77c7d8dde96ad079c2af21c2139aad08e03dc4b3
stianmorsund <stian.morsund@ks.no>
2025-05-05 21:49:17 +0200

    chore(react): define external deps

--- packages/react/vite.config.ts
+++ packages/react/vite.config.ts
@@ -6,11 +6,6 @@ import { defineConfig } from 'vite'
 import dts from 'vite-plugin-dts'
 import packageJson from './package.json'
 
-const dependencies = Object.keys({
-  ...packageJson.dependencies,
-  // ...packageJson.peerDependencies,
-})
-
 export default defineConfig(() => ({
   root: __dirname,
   cacheDir: '../../node_modules/.vite/packages/react',
@@ -22,10 +17,6 @@ export default defineConfig(() => ({
       tsconfigPath: path.join(__dirname, 'tsconfig.lib.json'),
     }),
   ],
-  // Uncomment this if you are using workers.
-  // worker: {
-  //  plugins: [ nxViteTsPaths() ],
-  // },
   // Configuration for building your library.
   // See: https://vitejs.dev/guide/build.html#library-mode
   build: {
@@ -40,13 +31,11 @@ export default defineConfig(() => ({
       entry: 'src/index.ts',
       name: '@ks-no/designsystem-react',
       fileName: 'index',
-      // Change this to the formats you want to support.
-      // Don't forget to update your package.json as well.
       formats: ['es' as const],
     },
     rollupOptions: {
       // External packages that should not be bundled into your library.
-      external: [...dependencies],
+      external: ['react', 'react-dom'],
     },
   },
   test: {
