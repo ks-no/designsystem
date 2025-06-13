6bfb956be8ff65c35964cc574f0e500896f004f1
stianmorsund <stian.morsund@ks.no>
2025-06-13 16:27:38 +0200

    chore: add testing-library

--- apps/angular-demo/src/app/app.spec.ts
+++ apps/angular-demo/src/app/app.spec.ts
@@ -1,25 +1,9 @@
-import { TestBed } from '@angular/core/testing'
-import { App } from './app'
+import { render, screen } from '@testing-library/angular';
+import { App } from './app';
 
-describe('App', () => {
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [App],
-    }).compileComponents()
-  })
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(App)
-    fixture.detectChanges()
-    const compiled = fixture.nativeElement as HTMLElement
-    expect(compiled.querySelector('h1')?.textContent).toContain(
-      'Welcome angular-demo',
-    )
-  })
-
-  it(`should have as title 'angular-demo'`, () => {
-    const fixture = TestBed.createComponent(App)
-    const app = fixture.componentInstance
-    expect(app.title).toEqual('angular-demo')
-  })
-})
+test('Should render title', async () => {
+  await render(App);
+  const title = screen.getByRole('heading', { name: 'Hi from Angular' })
+  expect(title).toBeTruthy()
+});
