37c6e828e66c38edfc5a6bf7b7707f7813c26f5f
stianmorsund <stian.morsund@ks.no>
2025-06-16 15:35:29 +0200

    chore(angular): lint

--- apps/angular-demo/src/app/app.spec.ts
+++ apps/angular-demo/src/app/app.spec.ts
@@ -1,7 +1,6 @@
 import { render, screen } from '@testing-library/angular';
 import { App } from './app';
 
-
 test('Should render title', async () => {
   await render(App);
   const title = screen.getByRole('heading', { name: 'Hi from Angular' })
