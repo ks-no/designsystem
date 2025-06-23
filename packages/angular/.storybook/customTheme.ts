df584c647b513bcc97b20d651744d5854ec4510d
stianmorsund <stian.morsund@ks.no>
2025-06-23 07:05:08 +0200

    chore(angular): storybook, custom-theme copied from digdir/designsystemet-repo

---
+++ packages/angular/.storybook/customTheme.ts
@@ -0,0 +1,15 @@
+import { create } from 'storybook/theming';
+
+export default create({
+  brandTitle: 'Komponenter',
+  fontBase: '"Inter", sans-serif',
+  // Colors
+  base: 'light',
+  colorSecondary: '#0062BA',
+  barTextColor: '#243142',
+  textColor: '#1e2b3c', // TODO does not work with css vars atm. bug?
+  inputTextColor: '#243142',
+  appBg: '#fefefe',
+  appPreviewBg: 'var(--ds-color-neutral-background-default)',
+  appContentBg: '#fefefe',
+});
