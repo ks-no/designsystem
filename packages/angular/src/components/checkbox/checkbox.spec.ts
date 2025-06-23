258e5816c5e0475b16c91f486e154b3b5fa4547e
stianmorsund <stian.morsund@ks.no>
2025-06-23 09:50:24 +0200

    feat(angular): `Checkbox` supports readonly

---
+++ packages/angular/src/components/checkbox/checkbox.spec.ts
@@ -0,0 +1,16 @@
+import { render, screen } from '@testing-library/angular';
+import userEvent from '@testing-library/user-event';
+import { Checkbox } from '../checkbox/checkbox';
+
+test('should not be clickable if readonly', async () => {
+    await render(
+        `
+        <input ksd-checkbox value="my-checkbox" readonly />
+    `,
+        { imports: [Checkbox] },
+    )
+
+    const checkbox = screen.getByRole('checkbox') as HTMLInputElement
+    await userEvent.click(checkbox)
+    expect(checkbox.checked).toBe(false)
+})
