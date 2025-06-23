0467a2a2314a62f97bf0960b8aefef8ff41696cf
stianmorsund <stian.morsund@ks.no>
2025-06-23 09:54:34 +0200

    chore(angular): add stories for checkbox

--- packages/angular/src/components/checkbox/checkbox.stories.ts
+++ packages/angular/src/components/checkbox/checkbox.stories.ts
@@ -6,20 +6,39 @@ import { FieldsetDescription } from '../fieldset/fieldset-description';
 import { FieldsetLegend } from '../fieldset/fieldset-legend';
 import { Label } from '../label/label';
 import { Checkbox } from './checkbox';
+import { CheckboxDescription } from './checkbox-description';
 
 const meta: Meta<Checkbox> = {
   component: Checkbox,
   title: 'Checkbox',
   decorators: [
     moduleMetadata({
-      imports: [Checkbox, Fieldset, FieldsetDescription, FieldsetLegend, Label, Field],
+      imports: [Checkbox, Label, Field, CheckboxDescription, Fieldset, FieldsetDescription, FieldsetLegend],
     })
   ]
 };
 export default meta;
 type Story = StoryObj<Checkbox>;
 
-export const Primary: Story = {
+export const Preview: Story = {
+  args: {},
+
+  render: (_args: any) => ({
+    template: `
+      <ksd-field>
+        <ksd-label> Checkbox label </ksd-label>
+        <input ksd-checkbox value="some-value"  />
+        <p ksd-checkbox-description>Description</p>
+      </ksd-field>
+    `,
+  }),
+  play: async ({ canvas }) => {
+    await expect(canvas.getByText(/Checkbox label/gi)).toBeTruthy()
+  },
+};
+
+
+export const Group: Story = {
   args: {},
 
   render: (_args: any) => ({
@@ -33,14 +52,67 @@ export const Primary: Story = {
       </p>
 
       <ksd-field>
-        <ksd-label> I can be chosen </ksd-label>
+        <ksd-label> E-post </ksd-label>
+        <input ksd-checkbox value="e-post"  />
+      </ksd-field>
+
+      <ksd-field>
+        <ksd-label> Telefon </ksd-label>
         <input ksd-checkbox value="telefon"  />
+      </ksd-field>
+
+      <ksd-field>
+        <ksd-label> SMS </ksd-label>
+        <input ksd-checkbox value="sms"  />
       </ksd-field>
 
     </fieldset>
     `,
   }),
-  play: async ({ canvas }) => {
-    await expect(canvas.getByText(/checkbox works!/gi)).toBeTruthy()
-  },
+};
+
+
+export const AriaLabel: Story = {
+  args: {},
+
+  render: (_args: any) => ({
+    template: `
+      <ksd-field>
+        <input ksd-checkbox value="some-value" aria-label="Checkbox label"  />
+      </ksd-field>
+    `,
+  }),
+};
+
+export const ReadOnly: Story = {
+  args: {},
+
+  render: (_args: any) => ({
+    template: `
+     <fieldset ksd-fieldset style="width: 400px;">
+      <legend ksd-fieldset-legend>
+        Hvordan vil du helst at vi skal kontakte deg?
+      </legend>
+      <p ksd-fieldset-description>
+        Velg alle alternativene som er relevante for deg.
+      </p>
+
+      <ksd-field>
+        <ksd-label> E-post </ksd-label>
+        <input readonly ksd-checkbox value="e-post"  />
+      </ksd-field>
+
+      <ksd-field>
+        <ksd-label> Telefon </ksd-label>
+        <input readonly ksd-checkbox value="telefon"  />
+      </ksd-field>
+
+      <ksd-field>
+        <ksd-label> SMS </ksd-label>
+        <input readonly ksd-checkbox value="sms"  />
+      </ksd-field>
+
+    </fieldset>
+    `,
+  }),
 };
