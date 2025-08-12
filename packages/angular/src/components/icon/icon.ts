65758d388558be9d50e4f06de1800fef45934bbd
stianmorsund <stian.morsund@ks.no>
2025-08-12 16:00:16 +0200

    chore(angular): cleanup some code

--- packages/angular/src/components/icon/icon.ts
+++ packages/angular/src/components/icon/icon.ts
@@ -8,27 +8,15 @@ import { iconRegistry } from './iconmap'
     :host {
       display: contents;
     }
-    :host svg {
-      width: var(--icon-size, 1em);
-      height: var(--icon-size, 1em);
-      fill: currentColor;
-    }
   `,
-  template: `
-    <span [style.--icon-size]="size()" [outerHTML]="trustedSVG()"></span>
-  `,
+  template: ` <span [outerHTML]="trustedSVG()"></span> `,
 })
 export class Icon {
   sanitizer = inject(DomSanitizer)
   icon = input('home')
-  size = input<string>('3em')
 
   protected svgIcon = computed(() => iconRegistry[this.icon()])
   protected trustedSVG = computed(() =>
     this.sanitizer.bypassSecurityTrustHtml(this.svgIcon()),
   )
-
-  constructor() {
-    console.log(this.size())
-  }
 }
--- packages/angular/src/components/icon/iconmap.ts
+++ packages/angular/src/components/icon/iconmap.ts
@@ -4,33 +4,20 @@ export const icons = import.meta.glob(
 )
 
 const formatSVG = (name: string, svg: string) => {
-  // Set the svg width=1em and height=1em
-  // Set aria-hidden=true
-  // Set focusable=false
-
-  // Handle data URLs
-
-  // Decode the data URL
   const decodedSvg = decodeURIComponent(svg.replace('data:image/svg+xml,', ''))
 
   const formattedSvg = decodedSvg
-    // First remove any existing attributes we want to replace
     .replace(/width=["'][^"']*["']/g, '')
     .replace(/height=["'][^"']*["']/g, '')
     .replace(/aria-hidden=["'][^"']*["']/g, '')
     .replace(/focusable=["'][^"']*["']/g, '')
-    // Then add our desired attributes
+    .replace(/fill=["'][^"']*["']/g, '')
+    // Add our desired attributes to the SVG tag
     .replace(
       /<svg([^>]*)>/g,
       '<svg$1 width="1em" height="1em" aria-hidden="true" focusable="false" fill="currentColor">',
     )
-    .replace(/<svg\b(?![^>]*fill=)/, '<svg fill="currentColor"')
 
-  if (name === 'home') {
-    console.log({ formattedSvg })
-  }
-  // Re-encode as data URL
-  // return `data:image/svg+xml,${encodeURIComponent(formattedSvg)}`
   return formattedSvg
 }
 
