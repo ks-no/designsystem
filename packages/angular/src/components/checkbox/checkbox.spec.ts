8c70a03904f374cc868de94c3340db1f3b6405bc
stianmorsund <stian.morsund@ks.no>
2025-08-15 11:51:16 +0200

    chore(angular): missing label for test

--- packages/angular/src/components/checkbox/checkbox.spec.ts
+++ packages/angular/src/components/checkbox/checkbox.spec.ts
@@ -2,15 +2,17 @@ import { render, screen } from '@testing-library/angular'
 import userEvent from '@testing-library/user-event'
 import { Field } from '../field/field'
 import { Input } from '../input/input'
+import { Label } from '../label/label'
 
 test('should not be clickable if readonly', async () => {
   await render(
     `
       <ksd-field>
+        <ksd-label>My checkbox</ksd-label>
         <input ksd-input type="checkbox" value="my-checkbox" readonly />
       </ksd-field>
     `,
-    { imports: [Field, Input] },
+    { imports: [Field, Input, Label] },
   )
 
   const checkbox = screen.getByRole('checkbox') as HTMLInputElement
