a5dd05e9e7d4a069214bcb66edcfc4f2d70b702d
stianmorsund <stian.morsund@ks.no>
2025-08-21 12:51:57 +0200

    chore(angular/card): storybook controls

--- packages/angular/src/components/card/card.stories.ts
+++ packages/angular/src/components/card/card.stories.ts
@@ -1,10 +1,27 @@
-import { moduleMetadata, type Meta, type StoryObj } from '@storybook/angular'
+import {
+  argsToTemplate,
+  moduleMetadata,
+  type Meta,
+  type StoryObj,
+} from '@storybook/angular'
+import { CommonArgs, commonArgTypes } from '../../../.storybook/default-args'
 import { Card } from './card'
 import { CardBlock } from './card.block'
 
-const meta: Meta<Card> = {
+type CardArgs = CommonArgs & {
+  variant: 'default' | 'tinted'
+}
+
+const meta: Meta<CardArgs> = {
   component: Card,
   title: 'Komponenter/Card',
+  argTypes: {
+    ...commonArgTypes,
+    variant: {
+      options: ['default', 'tinted'],
+      control: { type: 'radio' },
+    },
+  },
   decorators: [
     moduleMetadata({
       imports: [Card, CardBlock],
@@ -12,13 +29,14 @@ const meta: Meta<Card> = {
   ],
 }
 export default meta
-type Story = StoryObj<Card>
+type Story = StoryObj<CardArgs>
 
 export const Preview: Story = {
-  render: (_args) => ({
+  render: (args) => ({
+    props: args,
     template: `
 <div style="max-width: 320px;">
-  <article ksd-card>
+  <article ksd-card ${argsToTemplate(args)}>
     <h2>Card</h2>
     <p>Most provide as with carried business are much better more the perfected designer. Writing slightly explain desk unable at supposedly about this</p>
     <p data-size="sm">Footer text</p>
@@ -29,10 +47,11 @@ export const Preview: Story = {
 }
 
 export const CardWithBlocks: Story = {
-  render: (_args) => ({
+  render: (args) => ({
+    props: args,
     template: `
 <div style="max-width: 320px;">
-  <article ksd-card>
+  <article ksd-card ${argsToTemplate(args)}>
   <h2 ksd-card-block>Use blocks to section the card</h2>
   <p ksd-card-block>Most provide as with carried business are much better more the perfected designer. Writing slightly explain desk unable at supposedly about this</p>
   <p ksd-card-block>Valgfri fotnote</p>
@@ -43,22 +62,23 @@ export const CardWithBlocks: Story = {
 }
 
 export const ListOfCards: Story = {
-  render: (_args) => ({
+  render: (args) => ({
+    props: args,
     template: `
   <ul style="display: grid; grid-template-columns: repeat(2, minmax(0, 1fr)); gap: 1rem;">
-    <li ksd-card>
+    <li ksd-card ${argsToTemplate(args)}>
       <h2>Item 1</h2>
       <p>Description</p>
     </li>
-    <li ksd-card>
+    <li ksd-card ${argsToTemplate(args)}>
       <h2>Item 2</h2>
       <p>Description</p>
     </li>
-    <li ksd-card>
+    <li ksd-card ${argsToTemplate(args)}>
       <h2>Item 3</h2>
       <p>Description</p>
     </li>
-    <li ksd-card>
+    <li ksd-card ${argsToTemplate(args)}>
       <h2>Item 4</h2>
       <p>Description</p>
     </li>
@@ -68,23 +88,12 @@ export const ListOfCards: Story = {
 }
 
 export const AsLink: Story = {
-  render: (_args) => ({
+  render: (args) => ({
+    props: args,
     template: `
 <div style="max-width: 320px;">
-  <article ksd-card>
+  <article ksd-card ${argsToTemplate(args)}>
     <h2><a href="/">Whole card is clickable when link is present inside heading</a></h2>
-  </article>
-</div>
-    `,
-  }),
-}
-
-export const TintedVariant: Story = {
-  render: (_args) => ({
-    template: `
-<div style="max-width: 320px;">
-  <article ksd-card variant="tinted">
-    <p>Dette er et kort med tinted variant. Det har en annen bakgrunnsfarge.</p>
   </article>
 </div>
     `,
