e834cc1ec88823eddf9ce6703285a5935f559a74
Håkon Herrevold <hakonherrevold@Hakons-MacBook-Pro-2.local>
2025-10-30 10:54:31 +0100

    feat(angular/alert): added alert component

---
+++ packages/angular/src/components/alert/alert.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { CommonInputs } from '../common-inputs'
+
+@Component({
+  selector: 'ksd-alert',
+  template: `<ng-content />`,
+  host: {
+    class: 'ds-alert',
+    style: 'display: block;',
+  },
+  hostDirectives: [
+    {
+      directive: CommonInputs,
+      inputs: ['data-size', 'data-color'],
+    },
+  ],
+})
+export class Alert {}
