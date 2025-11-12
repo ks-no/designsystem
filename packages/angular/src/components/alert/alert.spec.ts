506c514615c20fe4951856d270075a511f69a2ab
stianmorsund <stian.morsund@ks.no>
2025-11-12 13:14:59 +0100

    test(alert): add test

---
+++ packages/angular/src/components/alert/alert.spec.ts
@@ -0,0 +1,33 @@
+import { NgStyle } from '@angular/common'
+import { render, screen } from '@testing-library/angular'
+import { Alert } from './alert'
+
+it('should render children, heading level 1', async () => {
+  await render(
+    `
+      <ksd-alert color="info">
+        <h1>Alert me!</h1>
+      </ksd-alert>
+    `,
+    { imports: [Alert] },
+  )
+
+  expect(screen.getByRole('heading', { level: 1 })).toHaveTextContent(
+    'Alert me!',
+  )
+})
+
+it('style should be applied', async () => {
+  await render(
+    `
+      <ksd-alert data-testid="alert" color="info" [ngStyle]="{ color: '#ffcc00' }" class="my-class">
+        <h1>Alert me!</h1>
+      </ksd-alert>
+    `,
+    { imports: [Alert, NgStyle] },
+  )
+
+  const alert = screen.getByTestId('alert')
+  expect(alert).toHaveStyle({ color: '#ffcc00' })
+  expect(alert).toHaveClass('my-class')
+})
