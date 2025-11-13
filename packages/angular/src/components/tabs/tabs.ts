a51944379ad0f139ad7781a21f1ae6df9db6e24d
Ronny Valentin Vindenes <ronny.vindenes@bouvet.no>
2025-11-13 12:27:31 +0100

    feat(angular): add tabs component

---
+++ packages/angular/src/components/tabs/tabs.ts
@@ -0,0 +1,93 @@
+import {
+  ChangeDetectionStrategy,
+  Component,
+  computed,
+  contentChildren,
+  effect,
+  input,
+  model,
+  signal,
+} from '@angular/core'
+import { CommonInputs } from '../common-inputs'
+import { TabsTab } from './tabs-tab'
+
+@Component({
+  selector: `ksd-tabs`,
+  template: `
+    <div class="ds-tabs">
+      <ng-content select="ksd-tabs-list" />
+      <ng-content select="ksd-tabs-panel" />
+    </div>
+  `,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  hostDirectives: [
+    {
+      directive: CommonInputs,
+      inputs: ['data-size', 'data-color'],
+    },
+  ],
+})
+export class Tabs {
+  readonly defaultValue = input<string>()
+  readonly value = model<string>('')
+  readonly focusedValue = computed(() =>
+    this.tabs()[this.focusedIndex()]?.value(),
+  )
+  readonly tabs = contentChildren(TabsTab, { descendants: true })
+  private focusedIndex = signal<number>(0)
+
+  constructor() {
+    effect(() => {
+      if (!this.value()) {
+        const value = this.defaultValue()
+        if (undefined !== value) {
+          this.changeTab(value)
+        }
+      }
+    })
+  }
+
+  onKeyDown(event: KeyboardEvent) {
+    switch (event.code) {
+      case 'ArrowLeft':
+        this.focusTab(
+          (this.focusedIndex() + this.tabs().length - 1) % this.tabs().length,
+        )
+        event.preventDefault()
+        break
+      case 'ArrowRight':
+        this.focusTab((this.focusedIndex() + 1) % this.tabs().length)
+        event.preventDefault()
+        break
+      case 'Enter':
+      case 'Space':
+        {
+          const value = this.focusedValue()
+          if (value) {
+            this.changeTab(value)
+          }
+        }
+        event.preventDefault()
+        break
+      case 'Home':
+        this.focusTab(0)
+        event.preventDefault()
+        break
+      case 'End':
+        this.focusTab(this.tabs().length - 1)
+        event.preventDefault()
+        break
+    }
+  }
+
+  public changeTab(value: string) {
+    const index = this.tabs().findIndex((tab) => tab.value() === value)
+    this.value.set(value)
+    this.focusTab(index)
+  }
+
+  private focusTab(index: number) {
+    this.focusedIndex.set(index)
+    this.tabs()[index]?.elementRef.nativeElement.focus()
+  }
+}
