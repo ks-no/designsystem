bbe3f4b475db6b825176c35a72cf44c79e504a53
stianmorsund <stian.morsund@ks.no>
2025-11-27 10:11:16 +0100

    refactor: field to `src`

--- packages/angular/field/src/field.ts
+++ packages/angular/field/src/field.ts
@@ -9,9 +9,9 @@ import {
   inject,
   input,
 } from '@angular/core'
-import { CommonInputs } from '@ks-digital/designsystem-angular/utils'
 import { Input } from '@ks-digital/designsystem-angular/input'
 import { Label } from '@ks-digital/designsystem-angular/label'
+import { CommonInputs } from '@ks-digital/designsystem-angular/utils'
 import { ValidationMessage } from '@ks-digital/designsystem-angular/validation-message'
 import { FieldCounter } from './field-counter'
 import { fieldObserver } from './field-observer'
