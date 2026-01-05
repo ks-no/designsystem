dc09de857e390bef5f4970a8238b1aa02021e358
stianmorsund <stian.morsund@ksdigital.no>
2026-01-05 15:24:47 +0100

    refactor: use u-tabs

--- packages/angular/tabs/src/tabs-list.ts
+++ packages/angular/tabs/src/tabs-list.ts
@@ -1,11 +1,18 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core'
+import {
+  ChangeDetectionStrategy,
+  Component,
+  CUSTOM_ELEMENTS_SCHEMA,
+} from '@angular/core'
+import '@u-elements/u-tabs'
 
 @Component({
   selector: `ksd-tabs-list`,
-  template: `<ng-content select="button[ksd-tabs-tab]" />`,
-  host: {
-    role: 'tablist',
-  },
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  template: `
+    <u-tablist>
+      <ng-content select="button[ksd-tabs-tab]" />
+    </u-tablist>
+  `,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class TabsList {}
--- packages/angular/tabs/src/tabs-tab.ts
+++ packages/angular/tabs/src/tabs-tab.ts
@@ -1,11 +1,8 @@
 import {
   ChangeDetectionStrategy,
   Component,
-  computed,
-  ElementRef,
-  inject,
+  CUSTOM_ELEMENTS_SCHEMA,
   input,
-  signal,
 } from '@angular/core'
 import {
   HostColor,
@@ -16,17 +13,13 @@ import { Tabs } from './tabs'
 
 @Component({
   selector: `button[ksd-tabs-tab]`,
-  template: ` <ng-content /> `,
-  host: {
-    role: 'tab',
-    class: 'ds-button',
-    '[id]': 'buttonId()',
-    '[attr.aria-controls]': 'ariaControls()',
-    '[attr.aria-selected]': 'isSelected()',
-    '[attr.tab-index]': 'isFocused() ? 0 : -1',
-    '(click)': 'tabs.changeTab(value())',
-    '(keydown)': 'tabs.onKeyDown($event)',
-  },
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  template: `<u-tab><ng-content /></u-tab>`,
+  host: {},
+  styles: `
+    :host {
+    }
+  `,
   changeDetection: ChangeDetectionStrategy.OnPush,
   imports: [],
   hostDirectives: [
@@ -45,14 +38,4 @@ export class TabsTab {
    * Unique value that will be set in the Tabs components state when the tab is activated
    */
   readonly value = input.required<string>()
-  readonly id = input<string>()
-  readonly elementRef = inject(ElementRef)
-  readonly ariaControls = signal<string | undefined>(undefined)
-  readonly buttonId = computed(() => this.id() ?? 'tab-' + randomId())
-
-  protected tabs = inject(Tabs)
-  protected isFocused = computed(
-    () => this.tabs.focusedValue() === this.value(),
-  )
-  protected isSelected = computed(() => this.tabs.value() === this.value())
 }
