760fea4d29cd7a488238ac7bc4a5d7b546dec582
stianmorsund <stian.morsund@ksdigital.no>
2026-01-07 14:20:30 +0100

    refactor: use types from designsystemet

--- packages/angular/__internals/src/index.ts
+++ packages/angular/__internals/src/index.ts
@@ -1,4 +1,3 @@
-export * from './colors' // Todo: replace with new types package from Designsystemet
 export { CommonInputs, Size } from './common-inputs'
 export { logIfDevMode } from './log-if-devmode'
 export { randomId } from './random-id'
