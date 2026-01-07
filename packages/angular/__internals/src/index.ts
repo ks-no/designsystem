67b70596d8067c71cd461925ddd6b130ed7a7e45
stianmorsund <stian.morsund@ksdigital.no>
2026-01-07 14:49:31 +0100

    fix(angular): add export to internals barrel

--- packages/angular/__internals/src/index.ts
+++ packages/angular/__internals/src/index.ts
@@ -1,3 +1,3 @@
-export { CommonInputs, Size } from './common-inputs'
+export { Color, CommonInputs, SeverityColors, Size } from './common-inputs'
 export { logIfDevMode } from './log-if-devmode'
 export { randomId } from './random-id'
