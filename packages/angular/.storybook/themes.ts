a85f3123f21246f05e5cbd7db8bd80813013a30f
stianmorsund <stian.morsund@ksdigital.no>
2026-01-07 10:22:25 +0100

    feat: add tilskudd theme to exports, storybook

--- packages/angular/.storybook/themes.ts
+++ packages/angular/.storybook/themes.ts
@@ -6,4 +6,5 @@ export const themes = [
   { name: 'Forvaltning', href: 'css/themes/forvaltning.css' },
   { name: 'Ledsagerbevis', href: 'css/themes/ledsagerbevis.css' },
   { name: 'Min kommune', href: 'css/themes/minkommune.css' },
+  { name: 'Tilskudd', href: 'css/themes/tilskudd.css' },
 ]
