f7a03eed1bb35c8af9c6aa442a33b0874f818a77
stianmorsund <stian.morsund@ksdigital.no>
2026-01-12 16:57:40 +0100

    fix: missing exports

--- packages/angular/__internals/src/index.ts
+++ packages/angular/__internals/src/index.ts
@@ -1,8 +1,10 @@
 export {
+  Color,
   CommonInputs,
   HostColor,
   HostSeverityColors,
   HostSize,
+  Size,
 } from './host-directives'
 export { logIfDevMode } from './log-if-devmode'
 export { randomId } from './random-id'
