d1b94b342d4d91b4f0500c9332173d431e60f9ec
stianmorsund <stian.morsund@ksdigital.no>
2026-01-12 16:28:04 +0100

    chore: remove comments

--- packages/angular/__internals/src/host-directives.ts
+++ packages/angular/__internals/src/host-directives.ts
@@ -1,15 +1,6 @@
 /* eslint-disable @angular-eslint/no-input-rename */
 
-/**
- * We use input aliasing to bridge the gap between Angular's camelCase property naming convention and our HTML data attributes.
- * This approach allows us to use valid HTML data attributes as documented by Designsystemet while maintaining
- * proper TypeScript intellisense support.
- *
- * Todo: Some components are using only a subset of colors, e.g., SeverityColors for Alert. We should reconsider this directive
- */
-
 import { Directive, input } from '@angular/core'
-
 import {
   ColorDefinitions,
   SeverityColorDefinitions,
