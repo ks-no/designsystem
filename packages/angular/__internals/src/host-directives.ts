dbf229b87b0566d3a76897dd9102a4082339a26f
stianmorsund <stian.morsund@ksdigital.no>
2026-01-13 15:56:25 +0100

    fix(angular): use types from themes

--- packages/angular/__internals/src/host-directives.ts
+++ packages/angular/__internals/src/host-directives.ts
@@ -4,10 +4,13 @@ import { Directive, input } from '@angular/core'
 import {
   ColorDefinitions,
   SeverityColorDefinitions,
-  Size as SizeDefinition,
-} from '@digdir/designsystemet/types'
+  SizeDefinition,
+} from '@ks-digital/designsystem-themes/types'
 
-export type { Color, SeverityColors, Size } from '@digdir/designsystemet/types'
+export type {
+  ColorDefinitions as Color,
+  SizeDefinition as Size,
+} from '@ks-digital/designsystem-themes/types'
 
 /**
  * @deprecated Use individual directives instead
@@ -23,16 +26,19 @@ export class CommonInputs {
    * Changes size for descendant Designsystemet components. Select from predefined sizes.
    * @attribute data-size
    */
-  dataSize = input<SizeDefinition>(undefined, { alias: 'data-size' })
+  dataSize = input<keyof SizeDefinition>(undefined, { alias: 'data-size' })
 
   /**
    * Changes color for descendant Designsystemet components.
    * Select from predefined colors and colors defined using theme.designsystemet.no.
    * @attribute data-color
    */
-  dataColor = input<ColorDefinitions>(undefined, { alias: 'data-color' })
+  dataColor = input<keyof ColorDefinitions>(undefined, { alias: 'data-color' })
 }
 
+/**
+ * Applies data-size attribute to host-element.
+ */
 @Directive({
   host: {
     '[attr.data-size]': 'dataSize() || null',
@@ -43,9 +49,12 @@ export class HostSize {
    * Changes size for descendant Designsystemet components. Select from predefined sizes.
    * @attribute data-size
    */
-  dataSize = input<SizeDefinition>(undefined, { alias: 'data-size' })
+  dataSize = input<keyof SizeDefinition>(undefined, { alias: 'data-size' })
 }
 
+/**
+ * Applies data-color attribute to host-element for all colors, including severity colors.
+ */
 @Directive({
   host: {
     '[attr.data-color]': 'dataColor() || null',
@@ -57,9 +66,15 @@ export class HostColor {
    * Select from predefined colors and colors defined using theme.designsystemet.no.
    * @attribute data-color
    */
-  dataColor = input<ColorDefinitions>(undefined, { alias: 'data-color' })
+  dataColor = input<keyof ColorDefinitions | keyof SeverityColorDefinitions>(
+    undefined,
+    { alias: 'data-color' },
+  )
 }
 
+/**
+ * Applies data-color attribute to host-element for severity colors.
+ */
 @Directive({
   host: {
     '[attr.data-color]': 'dataColor() || null',
@@ -71,7 +86,7 @@ export class HostSeverityColors {
    * Select from predefined colors and colors defined using theme.designsystemet.no.
    * @attribute data-color
    */
-  dataColor = input<SeverityColorDefinitions>(undefined, {
+  dataColor = input<keyof SeverityColorDefinitions>(undefined, {
     alias: 'data-color',
   })
 }
