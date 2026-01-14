d333fa49fca8f9ec3dca24c213cf6c2896e36d35
stianmorsund <stian.morsund@ksdigital.no>
2026-01-14 21:57:56 +0100

    chore: remove custom implementation

--- packages/angular/tabs/src/tabs.stories.ts
+++ packages/angular/tabs/src/tabs.stories.ts
@@ -4,19 +4,13 @@ import { Meta, moduleMetadata } from '@storybook/angular'
 import { CommonArgs, commonArgTypes } from '../../.storybook/default-args'
 import { Tabs, TabsList, TabsPanel, TabsTab } from './'
 
-type TabsArgs = CommonArgs & {
-  defaultValue?: string
-}
+type TabsArgs = CommonArgs
 
 const meta: Meta<TabsArgs> = {
   component: Tabs,
   title: 'Komponenter/Tabs',
   argTypes: {
     ...commonArgTypes,
-    defaultValue: {
-      control: { type: 'text' },
-      description: 'Default selected tab value',
-    },
   },
   decorators: [
     moduleMetadata({
@@ -29,27 +23,29 @@ export default meta
 type Story = Meta<Tabs>
 
 export const Preview: Story = {
-  args: { defaultValue: 'value1' },
   render: (args) => ({
     props: {
       ...args,
     },
     template: `
-<u-tabs class="ds-tabs">
-  <u-tablist>
-    <u-tab>Tab 1</u-tab>
-    <u-tab>Tab 2</u-tab>
-    <u-tab>Tab 3</u-tab>
-  </u-tablist>
-  <u-tabpanel>Panel 1 with <a href="#">link</a></u-tabpanel>
-  <u-tabpanel>Panel 2 with <a href="#">link</a></u-tabpanel>
-  <u-tabpanel>Panel 3 with <a href="#">link</a></u-tabpanel>
-</u-tabs>
 
-<br /><br />  
+   <ksd-tabs>
+        <ksd-tabs-list>
+          <ksd-tabs-tab value='value1'>Tab 1</ksd-tabs-tab>
+          <ksd-tabs-tab value='value2'>Tab 2</ksd-tabs-tab>
+        </ksd-tabs-list>
+        <ksd-tabs-panel value='value1'>content 1</ksd-tabs-panel>
+        <ksd-tabs-panel value='value2'>content 2</ksd-tabs-panel>
+      </ksd-tabs>
 
 
-      <ksd-tabs [defaultValue]='defaultValue'>
+      <br>
+
+
+
+
+
+      <ksd-tabs>
         <ksd-tabs-list>
            <ksd-tabs-tab>
             Tab 1
--- packages/angular/tabs/src/tabs.ts
+++ packages/angular/tabs/src/tabs.ts
@@ -2,13 +2,8 @@ import { NgTemplateOutlet } from '@angular/common'
 import {
   ChangeDetectionStrategy,
   Component,
-  computed,
   contentChildren,
   CUSTOM_ELEMENTS_SCHEMA,
-  effect,
-  input,
-  model,
-  signal,
 } from '@angular/core'
 import {
   HostColor,
@@ -27,7 +22,7 @@ import { TabsTab } from './tabs-tab'
       <u-tablist>
         @for (tab of tabs(); track tab) {
           <u-tab>
-            <ng-container *ngTemplateOutlet="tab.template()" />
+            <ng-container *ngTemplateOutlet="tab.templateRef()" />
           </u-tab>
         }
       </u-tablist>
@@ -47,69 +42,5 @@ import { TabsTab } from './tabs-tab'
   ],
 })
 export class Tabs {
-  readonly defaultValue = input<string>()
-  readonly value = model<string>('')
-  readonly selectedValue = computed(() =>
-    this.tabs()[this.selectedIndex()]?.value(),
-  )
   readonly tabs = contentChildren(TabsTab, { descendants: true })
-  private selectedIndex = signal<number>(0)
-
-  constructor() {
-    effect(() => {
-      if (!this.value()) {
-        const value = this.defaultValue()
-        if (undefined !== value) {
-          this.changeTab(value)
-        }
-      }
-    })
-  }
-
-  onKeyDown(event: KeyboardEvent) {
-    switch (event.code) {
-      case 'ArrowLeft':
-      case 'ArrowUp':
-        this.selectTab(
-          (this.selectedIndex() + this.tabs().length - 1) % this.tabs().length,
-        )
-        event.preventDefault()
-        break
-      case 'ArrowRight':
-      case 'ArrowDown':
-        this.selectTab((this.selectedIndex() + 1) % this.tabs().length)
-        event.preventDefault()
-        break
-      case 'Enter':
-      case 'Space':
-        {
-          const value = this.selectedValue()
-          console.log({ value })
-          if (value) {
-            this.changeTab(value)
-          }
-        }
-        event.preventDefault()
-        break
-      case 'Home':
-        this.selectTab(0)
-        event.preventDefault()
-        break
-      case 'End':
-        this.selectTab(this.tabs().length - 1)
-        event.preventDefault()
-        break
-    }
-  }
-
-  public changeTab(value: string) {
-    const index = this.tabs().findIndex((tab) => tab.value() === value)
-    this.value.set(value)
-    this.selectTab(index)
-  }
-
-  private selectTab(index: number) {
-    this.selectedIndex.set(index)
-    // this.tabs()[index]?.elementRef.nativeElement.focus()
-  }
 }
