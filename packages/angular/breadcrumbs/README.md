9e3fe252f3c55251e9f684d42c4c8e440ecf4b0c
stianmorsund <stian.morsund@ksdigital.no>
2026-01-20 14:52:58 +0100

    feat(angular): breadcrumbs

---
+++ packages/angular/breadcrumbs/README.md
@@ -0,0 +1,3 @@
+# @ks-digital/designsystem-angular/breadcrumbs
+
+Secondary entry point of `@ks-digital/designsystem-angular`. It can be used by importing from `@ks-digital/designsystem-angular/breadcrumbs`.
---
+++ packages/angular/breadcrumbs/src/breadcrumbs.spec.ts
@@ -0,0 +1,38 @@
+import { Link } from '@ks-digital/designsystem-angular/link'
+import { render, screen } from '@testing-library/angular'
+import { Breadcrumbs } from './breadcrumbs'
+
+const renderCrumbs = async () =>
+  await render(
+    `
+  <nav aria-label="Du er her:" class="ds-breadcrumbs">
+  <a ksd-link href="#" aria-label="Tilbake til Nivå 3">Nivå 3</a>
+  <ol>
+    <li>
+      <a ksd-link href="#">Nivå 1</a>
+    </li>
+    <li>
+      <a ksd-link href="#">Nivå 2</a>
+    </li>
+    <li>
+      <a ksd-link href="#">Nivå 3</a>
+    </li>
+    <li>
+      <a ksd-link href="#" aria-current="page">Nivå 4</a>
+    </li>
+  </ol>
+</nav>      
+    `,
+    { imports: [Link, Breadcrumbs] },
+  )
+
+it('should render breadcrumbs', async () => {
+  await renderCrumbs()
+
+  const crumbs = screen.getByRole('navigation')
+  expect(crumbs).toBeInTheDocument()
+  expect(crumbs).toHaveClass('ds-breadcrumbs')
+
+  const links = screen.getAllByRole('link')
+  expect(links).toHaveLength(5)
+})
