8a3339582ce82b35ac87f286b2878d9347ba1f12
stianmorsund <stian.morsund@ksdigital.no>
2026-01-26 14:58:32 +0100

    feat(angular): tag

---
+++ packages/angular/tag/src/tag.ts
@@ -0,0 +1,40 @@
+/* eslint-disable @angular-eslint/no-input-rename */
+import { Component, input } from '@angular/core'
+import {
+  HostColor,
+  HostSize,
+} from '@ks-digital/designsystem-angular/__internals'
+
+@Component({
+  selector: 'ksd-tag',
+  hostDirectives: [
+    {
+      directive: HostSize,
+      inputs: ['data-size'],
+    },
+    {
+      directive: HostColor,
+      inputs: ['data-color'],
+    },
+  ],
+  host: {
+    class: 'ds-tag',
+    '[attr.data-variant]': 'variant()',
+  },
+  styles: `
+    :host ::ng-deep > * {
+      margin-inline-end: var(--ds-size-1);
+    }
+  `,
+  template: ` <ng-content /> `,
+})
+export class Tag {
+  /**
+   * The visual variant of the tag
+   *
+   * @default 'default'
+   */
+  readonly variant = input<'default' | 'outline'>('default', {
+    alias: 'data-variant',
+  })
+}
