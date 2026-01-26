9cc7537bb33dfaa54158ea75e6027089da3f42bc
stianmorsund <stian.morsund@ksdigital.no>
2026-01-26 16:40:27 +0100

    chore(angular): support color-scheme in storybook (#208)

--- packages/angular/.storybook/preview.ts
+++ packages/angular/.storybook/preview.ts
@@ -6,6 +6,7 @@ import { themes } from './themes'
 
 addons.getChannel().on('globalsUpdated', ({ globals }) => {
   setTheme(globals.theme)
+  setColorScheme(globals.colorScheme)
 })
 
 function setTheme(href: string): void {
@@ -20,6 +21,13 @@ function setTheme(href: string): void {
     document.head.appendChild(link)
   }
   link.href = href
+}
+
+function setColorScheme(colorScheme: 'light' | 'dark' | 'auto'): void {
+  const stories = document.querySelectorAll('.docs-story')
+  stories.forEach((el) => {
+    el.setAttribute('data-color-scheme', colorScheme)
+  })
 }
 
 type ThemeGlobalType = {
@@ -44,6 +52,20 @@ export const globalTypes: Record<string, ThemeGlobalType> = {
       showName: true,
     },
   },
+  colorScheme: {
+    name: 'Color Scheme',
+    description: 'Set color scheme for components',
+    defaultValue: 'light',
+    toolbar: {
+      icon: 'moon',
+      items: [
+        { value: 'light', title: 'Light' },
+        { value: 'dark', title: 'Dark' },
+        { value: 'auto', title: 'Auto' },
+      ],
+      showName: true,
+    },
+  },
 }
 
 const preview: Preview = {
@@ -51,6 +73,11 @@ const preview: Preview = {
     applicationConfig({
       providers: [provideZonelessChangeDetection()],
     }),
+    (story, context) => {
+      const storyFn = story()
+      setColorScheme(context.globals['colorScheme'] || 'light')
+      return storyFn
+    },
   ],
   parameters: {
     actions: { argTypesRegex: '^on[A-Z].*' },
