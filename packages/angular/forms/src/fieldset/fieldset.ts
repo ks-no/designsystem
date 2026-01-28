6e59f6581b48f74a99eef32f670c34e228b92baa
stianmorsund <stian.morsund@ksdigital.no>
2026-01-28 14:38:00 +0100

    fix(angular): dont set role on `fieldset` (#217)

--- packages/angular/forms/src/fieldset/fieldset.ts
+++ packages/angular/forms/src/fieldset/fieldset.ts
@@ -3,7 +3,6 @@ import { Component } from '@angular/core'
 @Component({
   selector: 'fieldset[ksd-fieldset]',
   host: {
-    role: 'fieldset',
     class: 'ds-fieldset',
   },
   template: ` <ng-content /> `,
