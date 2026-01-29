c459034e3efa7a618943e80af9c08a3840428c6f
stianmorsund <stian.morsund@ksdigital.no>
2026-01-29 09:54:40 +0100

    fix(angular): dont set role on fieldset `legend`

--- packages/angular/forms/src/fieldset/fieldset-legend.ts
+++ packages/angular/forms/src/fieldset/fieldset-legend.ts
@@ -3,7 +3,6 @@ import { Component } from '@angular/core'
 @Component({
   selector: 'legend[ksd-fieldset-legend]',
   host: {
-    role: 'legend',
     class: 'ds-label',
   },
   template: ` <ng-content /> `,
