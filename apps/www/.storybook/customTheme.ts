4aeb896df41c9283d7503bff57084e2de91f51f5
stianmorsund <stian.morsund@ksdigital.no>
2026-02-17 17:13:46 +0100

    chore: working on theming

--- apps/www/.storybook/customTheme.ts
+++ apps/www/.storybook/customTheme.ts
@@ -5,11 +5,15 @@ export default create({
   fontBase: '"Inter", sans-serif',
   // Colors
   base: 'light',
+  brandUrl: 'https://your-site.com',
+  brandImage: '/logo.svg', // Place your logo in public or static folder
+  brandTarget: '_self',
+  colorPrimary: '#0055a6', // Your primary color
   colorSecondary: '#0062BA',
   barTextColor: '#243142',
   textColor: '#1e2b3c',
   inputTextColor: '#243142',
-  appBg: '#fefefe',
+  appBg: '#e8e8ec',
   appPreviewBg: 'var(--ds-color-neutral-background-default)',
   appContentBg: '#fefefe',
 })
