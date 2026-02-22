f4e034b49e7d34f4b855a45414691b186ec219c9
stianmorsund <stian.morsund@ksdigital.no>
2026-02-22 20:03:58 +0100

    feat: build themes

--- packages/themes/src/themes/colors.d.ts
+++ packages/themes/src/themes/colors.d.ts
@@ -1,9 +1,9 @@
 /* This file is deprecated and will be removed in a future release. Use types.d.ts instead */
-/* build: v1.7.3 */
-import type {} from '@digdir/designsystemet/types'
+/* build: v1.11.1 */
+import type {} from '@digdir/designsystemet-types'
 
 // Augment types based on theme
-declare module '@digdir/designsystemet/types' {
+declare module '@digdir/designsystemet-types' {
   export interface ColorDefinitions {
     accent: never
     support1: never
--- packages/themes/src/themes/types.d.ts
+++ packages/themes/src/themes/types.d.ts
@@ -1,17 +1,17 @@
-/* build: v1.7.3 */
-import type {} from '@digdir/designsystemet/types'
-export type { Size } from '@digdir/designsystemet/types'
+/* build: v1.11.1 */
+import type {} from '@digdir/designsystemet-types'
 
 // Augment types based on theme
-
-export interface ColorDefinitions {
-  accent: never
-  support1: never
-  neutral: never
-}
-export interface SeverityColorDefinitions {
-  info: never
-  success: never
-  warning: never
-  danger: never
+declare module '@digdir/designsystemet-types' {
+  export interface ColorDefinitions {
+    accent: never
+    support1: never
+    neutral: never
+  }
+  export interface SeverityColorDefinitions {
+    info: never
+    success: never
+    warning: never
+    danger: never
+  }
 }
