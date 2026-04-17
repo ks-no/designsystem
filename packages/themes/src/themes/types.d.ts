26bceb212e74a98d0b3482c90bc2e118b0c67944
stianmorsund <stian.morsund@ksdigital.no>
2026-04-17 13:09:05 +0200

    types

--- packages/themes/src/themes/types.d.ts
+++ packages/themes/src/themes/types.d.ts
@@ -1,17 +1,16 @@
 /* build: v1.12.1 */
 import type {} from '@digdir/designsystemet-types'
+export type { Size } from '@digdir/designsystemet-types'
 
 // Augment types based on theme
-declare module '@digdir/designsystemet-types' {
-  export interface ColorDefinitions {
-    accent: never
-    support1: never
-    neutral: never
-  }
-  export interface SeverityColorDefinitions {
-    info: never
-    success: never
-    warning: never
-    danger: never
-  }
+export interface ColorDefinitions {
+  accent: never
+  support1: never
+  neutral: never
+}
+export interface SeverityColorDefinitions {
+  info: never
+  success: never
+  warning: never
+  danger: never
 }
