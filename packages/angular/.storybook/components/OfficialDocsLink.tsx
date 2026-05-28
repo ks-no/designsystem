577c785df736c89c70ffc2f985ee932abc9e3fb3
stianmorsund <stian.morsund@ksdigital.no>
2026-05-28 15:33:33 +0200

    fix(themes): move `Alert`-icon overrides to themes-package (#293)

--- packages/angular/.storybook/components/OfficialDocsLink.tsx
+++ packages/angular/.storybook/components/OfficialDocsLink.tsx
@@ -1,18 +1,31 @@
 interface Props {
   href: string
-  label?: string
+  component: string
 }
 
-export function OfficialDocsLink({ href, label }: Props) {
+export function OfficialDocsLink({ href, component }: Props) {
   return (
-    <a
-      href={href}
-      target="_blank"
-      rel="noopener noreferrer"
-      className="official-docs"
-    >
-      <span aria-hidden="true">📖</span>{' '}
-      {label ?? 'Besøk Digdir sin dokumentasjon'}
-    </a>
+    <div className="sb-unstyled ds-alert">
+      <h2
+        className="sb-unstyled ds-heading"
+        data-size="sm"
+        style={{
+          paddingBottom: 'var(--ds-size-2) !important',
+        }}
+      >
+        Om dokumentasjonen for {component}
+      </h2>
+      Retningslinjer for komponenten finnes på Designsystemet.no. Denne siden
+      inneholder kun Angular-spesifikk informasjon for å unngå duplikat
+      dokumentasjon. <br />
+      <a
+        href={href}
+        target="_blank"
+        rel="noopener noreferrer"
+        className="sb-unstyled ds-link"
+      >
+        <span aria-hidden="true">📖</span> {`Besøk Digdir sin dokumentasjon`}
+      </a>
+    </div>
   )
 }
