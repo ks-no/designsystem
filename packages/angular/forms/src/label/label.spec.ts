7e65fa40f27e4b576a3bcbf4256a86060e23f24e
stianmorsund <stian.morsund@ksdigital.no>
2026-06-15 11:50:13 +0200

    remove a11y test for label, handled by field

--- packages/angular/forms/src/label/label.spec.ts
+++ packages/angular/forms/src/label/label.spec.ts
@@ -1,6 +1,4 @@
 import { render, screen } from '@testing-library/angular'
-import { axe } from 'vitest-axe'
-import { Input } from '../input'
 import { Label } from './label'
 
 it('should render label', async () => {
@@ -8,17 +6,4 @@ it('should render label', async () => {
 
   const label = screen.getByText('My label')
   expect(label).toBeInTheDocument()
-})
-
-it('should have no obvious accessibility violations', async () => {
-  const { container } = await render(
-    `
-    <ksd-label>My input label</ksd-label>
-    <input ksd-input id="my-input" type="text" />
-    `,
-    { imports: [Label, Input] },
-  )
-
-  const results = await axe(container)
-  expect(results).toHaveNoViolations()
 })
