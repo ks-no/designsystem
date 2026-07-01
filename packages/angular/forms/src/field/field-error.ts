ac9e74be04a40dbba9ecf32db2e4c2e5121819b1
stianmorsund <stian.morsund@ksdigital.no>
2026-07-01 13:14:18 +0200

    fix(angular): expand ksd-error selector (#314)

--- packages/angular/forms/src/field/field-error.ts
+++ packages/angular/forms/src/field/field-error.ts
@@ -2,7 +2,7 @@ import { ChangeDetectionStrategy, Component } from '@angular/core'
 import { ValidationMessage } from '@ks-digital/designsystem-angular/validation-message'
 
 @Component({
-  selector: '[ksd-error]',
+  selector: 'ksd-error, [ksd-error]',
   changeDetection: ChangeDetectionStrategy.OnPush,
   template: `<ng-content />`,
   hostDirectives: [
