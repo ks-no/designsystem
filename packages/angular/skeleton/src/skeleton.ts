bca1e2d4010d7f724e4e8be2ecd3df1eb532c25d
stianmorsund <stian.morsund@ksdigital.no>
2026-07-03 13:49:58 +0200

    feat(angular): skeleton

---
+++ packages/angular/skeleton/src/skeleton.ts
@@ -0,0 +1,41 @@
+import {
+  ChangeDetectionStrategy,
+  Component,
+  computed,
+  input,
+} from '@angular/core'
+
+/**
+ * Skeleton is used to represent a draft of page while the content loads.
+ */
+@Component({
+  selector: 'ksd-skeleton',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  template: ` <ng-content />`,
+  host: {
+    class: 'ds-skeleton',
+    '[attr.data-variant]': 'variant()',
+    '[attr.data-text]': 'dataText()',
+    'aria-hidden': 'true',
+  },
+  styles: `
+    :host {
+      display: block;
+    }
+  `,
+})
+export class Skeleton {
+  /**
+   * Specify which variant to use
+   * @default 'rectangle'
+   */
+  readonly variant = input<'rectangle' | 'circle' | 'text'>('rectangle', {
+    alias: 'data-variant',
+  })
+
+  readonly isText = computed(() => this.variant() === 'text')
+
+  readonly dataText = computed(() => {
+    return this.isText() ? '-' : null
+  })
+}
